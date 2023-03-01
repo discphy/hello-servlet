d5bb7a5c23724d7f3a6e940746bdc4c2cb7a31cc
discphy <dinophy@nate.com>
2023-03-02 08:44:13 +0900

    feat(add): Separate view

--- src/main/java/hello/servlet/web/frontcontroller/v1/ControllerV1.java
+++ src/main/java/hello/servlet/web/frontcontroller/v1/ControllerV1.java
@@ -8,6 +8,4 @@ import java.io.IOException;
 public interface ControllerV1 {
 
     void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException;
-
-
 }
